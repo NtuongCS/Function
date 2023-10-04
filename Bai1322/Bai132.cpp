6b0bbb3f4eea4ad77abd9fdca2308c2c193e676e
NtuongCS <phamnguyentuong205@gmail.com>
2023-10-04 18:37:45 +0700

    4/10

--- Bai1322/Bai132.cpp
+++ Bai1322/Bai132.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void nhap(float&, float&);
 void xuat(float, float);
-double dientich(float, float, float,float,float,float);
+double dientich(float, float, float, float, float, float);
 void kiemtra(float, float, float, float);
 int main()
 {
@@ -25,14 +25,14 @@ int main()
 	xuat(xC, yC);
 	cout << "\n";
 	float xM, yM;
-	nhap(xM,yM);
+	nhap(xM, yM);
 	cout << "\nDiem  M: ";
 	xuat(xM, yM);
 	cout << "\n";
-    double sABC = dientich(xA, yA, xB, yB,xC,yC);
+	double sABC = dientich(xA, yA, xB, yB, xC, yC);
 	double sMAB = dientich(xA, yA, xB, yB, xM, yM);
-	double sMBC= dientich(xB, yB, xC, yC,xM,yM);
-	double sMAC = dientich(xA,yA,xC, yC,xM,yM);
+	double sMBC = dientich(xB, yB, xC, yC, xM, yM);
+	double sMAC = dientich(xA, yA, xC, yC, xM, yM);
 
 }
 void nhap(float& xx, float& yy)
@@ -46,17 +46,4 @@ void xuat(float xx, float yy)
 {
 	cout << "Hoanh do: " << xx;
 	cout << "Tung do: " << yy;
-}
-
-
-double dientich(float xA, float yA, float xB,float yB,float xC,float yC)
-{
-	return  0.5 * abs(xA * yB + xB * yC + xC * yA - xB * yA - xC * yB - xA * yC);
-}
-void kiemTra(float sABC, float sMAB, float sMAC, float sMBC)
-{
-	if (sABC==sMAB + sMBC + sMAC)
-		cout << "M trong";
-	else
-		cout << "M ngoai";
 }
